3b29ec46a3f087fc8ede05e76adff8d1f4ef1e73
wsgeorge <williamsaintgeorge@gmail.com>
2015-01-28 09:52:07 +0000

    add topic serializer to resource

--- project/apps/lectures/serializers.py
+++ project/apps/lectures/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
+from apps.core.serializers import TopicSerializer
 from .models import LearningResource
 
 class LearningResourceSerializer(serializers.HyperlinkedModelSerializer):
-
+	topic = TopicSerializer(many=False, read_only=True)
+	
 	class Meta:
 		model = LearningResource
 		fields = ('id', 'url', 'link', 'topic' ,'summary', 'name',)
